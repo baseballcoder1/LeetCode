124fa8d054194f146298f45e8d4f0f0e0978643b
baseballcoder1 <154934118+baseballcoder1@users.noreply.github.com>
2024-03-13 15:44:06 +0900

    Add files via upload

--- 2485. Find the Pivot Integer.py
+++ 2485. Find the Pivot Integer.py
@@ -10,10 +10,16 @@ Difficulty: Easy
 
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        for i in range(1, n+1):
-            sum1 = i*(1+i)/2
-            sum2 = (n-i+1)*(i+n)/2
-            if sum1 == sum2:
-                return i
+        def sqrt(x):
+            for i in range(1, x+1):
+                y = i*i
+                if y == x:
+                    return i
+                elif y > x:
+                    return None
+
+        x = sqrt(n*(n+1)//2)
+        if x is not None:
+            return x
         else:
             return -1
